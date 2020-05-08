2d727e58294f3263cdd230c8fee3c189aaae820d
Ivasha91 <volegovivan91@gmail.com>
2020-05-08 15:17:47 +0700

    phoneNav

--- js/main.js
+++ js/main.js
@@ -11,4 +11,17 @@ function scrollToKatalog() {
   $('html, body').animate({
         scrollTop: $('#cards').offset().top
     }, 500);
+};
+
+let p = heightNavPhone();
+function heightNavPhone() {
+  let y = $('.navbar').outerHeight();
+  let z = $('#phoneNav').height();
+ // let x = $('.navbar').css('padding-top');
+ return (y - z)/2 ;
 }
+
+$('#phoneNav').css('padding-top', p).css('padding-bottom', p)
+
+
+// TODO: height что то про саженцы = height картинки
